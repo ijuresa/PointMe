53abde58aa0e94ae9c2f6c7a0ad7694050553ded
Ivan Juresa <ivanjuresa611@gmail.com>
2016-10-23 21:33:45 +0200

    Initial Commit. OpenCV working

---
+++ app/src/main/java/com/example/ivanj/pointme/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ivanj.pointme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "Not working");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "It's Alive");
+        }
+
+    }
+}
