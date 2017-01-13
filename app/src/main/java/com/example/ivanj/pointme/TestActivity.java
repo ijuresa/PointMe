8cdbbaa852a0fa3b6f21c7f48b068e220190c3cd
Ivan Juresa <ivanjuresa611@gmail.com>
2017-01-13 15:55:27 +0100

    Buttons and their actions, Simulated touch event
    * Added: Buttons which serve as keyboard
    Each button acts as one letter from alphabet
    Their on touch events are connected to Instrumentation object which
    emulates keystroke
    * Created better calibration, although it needs more tweaking
    * Fixed: Problem where pointer would go beneath buttons

--- app/src/main/java/com/example/ivanj/pointme/TestActivity.java
+++ app/src/main/java/com/example/ivanj/pointme/TestActivity.java
@@ -19,9 +19,14 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -32,6 +37,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -57,9 +65,11 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
 
     //Pointer variables
     private TranslateAnimation movePointer;
-    ImageView pointerImage;
+    private ImageView pointerImage;
     private Point centerPoint = new Point(0,0);
-    Point oldCenterPoint;
+    private Point oldCenterPoint;
+
+    //For multiplication threshold
     private double areaThreshold = 0.25;
 
     private CameraBridgeViewBase mOpenCvCameraView;
@@ -69,6 +79,11 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
 
     //Values from SharedPreferences
     double maxArea, minArea;
+
+
+    final Instrumentation m_Instrumentation = new Instrumentation();
+
+    View.OnClickListener globalButtonListener;
 
 
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
@@ -101,11 +116,18 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
         //Pointer image
         pointerImage = (ImageView)findViewById(R.id.imageViewPoint);
 
+        //EditText
+        EditText inputText = (EditText)findViewById(R.id.editText);
+        //Don't show keyboard when text is in focus
+        inputText.setShowSoftInputOnFocus(false);
+
+        //Init Buttons
+        initButtons();
+
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setAlpha(0);
-
     }
     @Override
     public void onPause() {
@@ -133,6 +155,11 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
+
+    /**
+     * @name getData
+     * @description Read data from SharedPreferences file and save to object for later usage
+     */
     public void getData() {
         //Get data
         SharedPreferences pSettings = getSharedPreferences(PREFERENCE_FILE, 0);
@@ -222,10 +249,10 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
         return false;
     }
 
-
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
         Log.i(ActivityTags.getActivity().getColorBlobDetection(), "mRgba " + mRgba);
+
         //Rotate screen by 90 degrees counter clockwise
         //TODO: CHECK this rotation, doesn't work always well
         Core.transpose(mRgba, gRbgaT);
@@ -253,10 +280,25 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
             double lowerBound = area1 * 1 - areaThreshold;
             double upperBound = area1 * 1 + areaThreshold;
 
-            //Click detected
-            //TODO: Add actions
+            //Click detected, two blobs of the aprox same size
             if((area2 <= upperBound) || (area2 >= lowerBound)) {
+                //Obtain MotionEvent object
+                //TODO: Remove when calibration will be done
+                long downTime = SystemClock.uptimeMillis();
+                long eventTime = SystemClock.uptimeMillis() + 100;
 
+                //40 and 750 are temporary calibration values
+                //TODO: Better automatic calibration -> Make user touch screen 4 points
+                float x = (float)centerPoint.x + 40;
+                float y = (float)centerPoint.y + 750;
+
+               Log.d(ActivityTags.getActivity().getColorBlobDetection(), "Touch found at x: " +
+               centerPoint.x + " and y: " + centerPoint.y);
+
+                m_Instrumentation.sendPointerSync(MotionEvent.obtain(SystemClock.uptimeMillis(),
+                        SystemClock.uptimeMillis(),MotionEvent.ACTION_DOWN,x, y, 0));
+                m_Instrumentation.sendPointerSync(MotionEvent.obtain(SystemClock.uptimeMillis(),
+                        SystemClock.uptimeMillis(),MotionEvent.ACTION_UP,x, y, 0));
             }
         } else {
             //Will be used to check if point has moved
@@ -280,9 +322,250 @@ public class TestActivity extends AppCompatActivity implements View.OnTouchListe
                     }
                 });
             }
-
         }
         return mRgba;
+    }
+
+    /**
+     * @name pressedButtonThread
+     * @param e: Integer containing KeyEvent code
+     * @description Starts new thread which passes argument e as keystroke
+     */
+    void pressedButtonThread(final int e) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                m_Instrumentation.sendKeyDownUpSync(e);
+            }
+        }).start();
+    }
+
+    /**
+     * @name initButtons
+     * @description Initializes buttons which serve as keyboard
+     *              Initializes OnClickListener for them
+     */
+    void initButtons() {
+        globalButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case (R.id.buttonQ):
+                            pressedButtonThread(KeyEvent.KEYCODE_Q);
+                        break;
+
+                    case (R.id.buttonW):
+                            pressedButtonThread(KeyEvent.KEYCODE_W);
+                        break;
+
+                    case (R.id.buttonE):
+                            pressedButtonThread(KeyEvent.KEYCODE_E);
+                        break;
+
+                    case (R.id.buttonR):
+                            pressedButtonThread(KeyEvent.KEYCODE_R);
+                        break;
+
+                    case (R.id.buttonT):
+                            pressedButtonThread(KeyEvent.KEYCODE_T);
+                        break;
+
+                    case (R.id.buttonY):
+                            pressedButtonThread(KeyEvent.KEYCODE_Y);
+                        break;
+
+                    case (R.id.buttonU):
+                            pressedButtonThread(KeyEvent.KEYCODE_U);
+                        break;
+
+                    case (R.id.buttonI):
+                            pressedButtonThread(KeyEvent.KEYCODE_I);
+                        break;
+
+                    case (R.id.buttonO):
+                            pressedButtonThread(KeyEvent.KEYCODE_O);
+                        break;
+
+                    case (R.id.buttonP):
+                            pressedButtonThread(KeyEvent.KEYCODE_P);
+                        break;
+
+                    case (R.id.buttonA):
+                            pressedButtonThread(KeyEvent.KEYCODE_A);
+                        break;
+
+                    case (R.id.buttonS):
+                            pressedButtonThread(KeyEvent.KEYCODE_S);
+                        break;
+
+                    case (R.id.buttonD):
+                            pressedButtonThread(KeyEvent.KEYCODE_D);
+                        break;
+
+                    case (R.id.buttonF):
+                            pressedButtonThread(KeyEvent.KEYCODE_F);
+                        break;
+
+                    case (R.id.buttonG):
+                            pressedButtonThread(KeyEvent.KEYCODE_G);
+                        break;
+
+                    case (R.id.buttonH):
+                            pressedButtonThread(KeyEvent.KEYCODE_H);
+                        break;
+
+                    case (R.id.buttonJ):
+                            pressedButtonThread(KeyEvent.KEYCODE_J);
+                        break;
+
+                    case (R.id.buttonK):
+                            pressedButtonThread(KeyEvent.KEYCODE_K);
+                        break;
+
+                    case (R.id.buttonL):
+                            pressedButtonThread(KeyEvent.KEYCODE_L);
+                        break;
+
+                    case (R.id.buttonZ):
+                            pressedButtonThread(KeyEvent.KEYCODE_Z);
+                        break;
+
+                    case (R.id.buttonX):
+                            pressedButtonThread(KeyEvent.KEYCODE_X);
+                        break;
+
+                    case (R.id.buttonC):
+                            pressedButtonThread(KeyEvent.KEYCODE_C);
+                        break;
+
+                    case (R.id.buttonV):
+                            pressedButtonThread(KeyEvent.KEYCODE_V);
+                        break;
+
+                    case (R.id.buttonB):
+                            pressedButtonThread(KeyEvent.KEYCODE_B);
+                        break;
+
+                    case (R.id.buttonN):
+                            pressedButtonThread(KeyEvent.KEYCODE_N);
+                        break;
+
+                    case (R.id.buttonM):
+                            pressedButtonThread(KeyEvent.KEYCODE_M);
+                        break;
+
+                    case (R.id.buttonBcksp):
+                            pressedButtonThread(KeyEvent.KEYCODE_DEL);
+                        break;
+
+                    case (R.id.buttonComma):
+                            pressedButtonThread(KeyEvent.KEYCODE_COMMA);
+                        break;
+
+                    case (R.id.buttonSpace):
+                            pressedButtonThread(KeyEvent.KEYCODE_SPACE);
+                        break;
+
+                    case (R.id.buttonDot):
+                            pressedButtonThread(KeyEvent.KEYCODE_PERIOD);
+                        break;
+                    }
+                }
+            };
+
+        //First row
+        Button buttonQ = (Button)findViewById(R.id.buttonQ);
+        buttonQ.setOnClickListener(globalButtonListener);
+
+        Button buttonW = (Button)findViewById(R.id.buttonW);
+        buttonW.setOnClickListener(globalButtonListener);
+
+        Button buttonE = (Button)findViewById(R.id.buttonE);
+        buttonE.setOnClickListener(globalButtonListener);
+
+        Button buttonR = (Button)findViewById(R.id.buttonR);
+        buttonR.setOnClickListener(globalButtonListener);
+
+        Button buttonT = (Button)findViewById(R.id.buttonT);
+        buttonT.setOnClickListener(globalButtonListener);
+
+        Button buttonY = (Button)findViewById(R.id.buttonY);
+        buttonY.setOnClickListener(globalButtonListener);
+
+        Button buttonU = (Button)findViewById(R.id.buttonU);
+        buttonU.setOnClickListener(globalButtonListener);
+
+        Button buttonI = (Button)findViewById(R.id.buttonI);
+        buttonI.setOnClickListener(globalButtonListener);
+
+        Button buttonO = (Button)findViewById(R.id.buttonO);
+        buttonO.setOnClickListener(globalButtonListener);
+
+        Button buttonP = (Button)findViewById(R.id.buttonP);
+        buttonP.setOnClickListener(globalButtonListener);
+
+        //Second row
+        Button buttonA = (Button)findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(globalButtonListener);
+
+        Button buttonS = (Button)findViewById(R.id.buttonS);
+        buttonS.setOnClickListener(globalButtonListener);
+
+        Button buttonD = (Button)findViewById(R.id.buttonD);
+        buttonD.setOnClickListener(globalButtonListener);
+
+        Button buttonF = (Button)findViewById(R.id.buttonF);
+        buttonF.setOnClickListener(globalButtonListener);
+
+        Button buttonG = (Button)findViewById(R.id.buttonG);
+        buttonG.setOnClickListener(globalButtonListener);
+
+        Button buttonH = (Button)findViewById(R.id.buttonH);
+        buttonH.setOnClickListener(globalButtonListener);
+
+        Button buttonJ = (Button)findViewById(R.id.buttonJ);
+        buttonJ.setOnClickListener(globalButtonListener);
+
+        Button buttonK = (Button)findViewById(R.id.buttonK);
+        buttonK.setOnClickListener(globalButtonListener);
+
+        Button buttonL = (Button)findViewById(R.id.buttonL);
+        buttonL.setOnClickListener(globalButtonListener);
+
+        //Third row
+        Button buttonZ = (Button)findViewById(R.id.buttonZ);
+        buttonZ.setOnClickListener(globalButtonListener);
+
+        Button buttonX = (Button)findViewById(R.id.buttonX);
+        buttonX.setOnClickListener(globalButtonListener);
+
+        Button buttonC = (Button)findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(globalButtonListener);
+
+        Button buttonV = (Button)findViewById(R.id.buttonV);
+        buttonV.setOnClickListener(globalButtonListener);
+
+        Button buttonB = (Button)findViewById(R.id.buttonB);
+        buttonB.setOnClickListener(globalButtonListener);
+
+        Button buttonN = (Button)findViewById(R.id.buttonN);
+        buttonN.setOnClickListener(globalButtonListener);
+
+        Button buttonM = (Button)findViewById(R.id.buttonM);
+        buttonM.setOnClickListener(globalButtonListener);
+
+        Button buttonBcksp = (Button)findViewById(R.id.buttonBcksp);
+        buttonBcksp.setOnClickListener(globalButtonListener);
+
+        //Fourth row
+        Button buttonComma = (Button)findViewById(R.id.buttonComma);
+        buttonComma.setOnClickListener(globalButtonListener);
+
+        Button buttonSpace = (Button)findViewById(R.id.buttonSpace);
+        buttonSpace.setOnClickListener(globalButtonListener);
+
+        Button buttonDot = (Button)findViewById(R.id.buttonDot);
+        buttonQ.setOnClickListener(globalButtonListener);
     }
 
     private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
