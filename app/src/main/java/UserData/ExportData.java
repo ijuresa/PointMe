262ecb7f530170eaabbfa21efb340f82f9c887dd
Ivan Juresa <ivanjuresa611@gmail.com>
2017-01-23 12:59:15 +0100

    Fixed - .csv data overwrite, Added - comments
    *Fixed* bug when if .csv file would exist from before, new data write
    would overwrite it completely
    *Added* Better code comments

--- app/src/main/java/UserData/ExportData.java
+++ app/src/main/java/UserData/ExportData.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 /**
  * Created by ivanj on 20/01/2017.
+ * @Description Class used to write data to .csv file
  */
 
 public class ExportData {
@@ -17,12 +18,18 @@ public class ExportData {
     private String fileName = "AnalysisData.csv";
     private String filePath = baseDir + File.separator + fileName;
 
-    File f = new File(filePath );
+    File f = new File(filePath);
     CSVWriter writer;
 
+    /**
+     * @Description Constructor of ExportData class
+     *              Checks for file "AnalysisData.csv" and if it doesn't exist it creates new one
+     *              with headers. Else it just appends data to existing .csv file
+     * @throws IOException
+     */
     private ExportData() throws IOException {
         if(f.exists() && !f.isDirectory()){
-            FileWriter mFileWriter = new FileWriter(filePath);
+            FileWriter mFileWriter = new FileWriter(filePath, true);
             writer = new CSVWriter(mFileWriter);
         } else {
             writer = new CSVWriter(new FileWriter(filePath));
@@ -30,6 +37,11 @@ public class ExportData {
         }
     }
 
+    /**
+     * @Description This function is called when file "AnalysisData.csv" is not found on drive
+     *              It writes headers to .csv file for further data allocation
+     * @throws IOException
+     */
     private void setHeaders() throws IOException {
         List<String[]> data = new ArrayList<String[]>();
         data.add(new String[] {"Timestamp", "User", "Age", "Gender", "Phrase ID", "Phrase Length",
@@ -38,6 +50,21 @@ public class ExportData {
         writer.close();
     }
 
+    /**
+     * @Description Called from main "TestActivity.java" when some parameters are met
+     *                  a) InputUser string must be equal with User string - then it writes data
+     *                      to .csv file
+     *
+     * @param _user         Name of the user
+     * @param _age          Age of the user
+     * @param _gender       Gender of the user
+     * @param _index        Index of input string
+     * @param _length       Length of the input string
+     * @param _wBackspace   Number of wrong backspaces
+     * @param _tBackspaces  Number of total backspaces
+     * @param _time         Time needed to write string
+     * @throws IOException
+     */
     public void writeRow(String _user, String _age, String _gender, int _index, int _length,
                          int _wBackspace, int _tBackspaces, long _time) throws IOException {
         writer = new CSVWriter(new FileWriter(filePath, true));
@@ -61,5 +88,4 @@ public class ExportData {
     }
 
     public static ExportData get_exportData() { return _exportData; }
-
 }
